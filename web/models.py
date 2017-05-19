dbf2ff4fcc21987d0415a068161532380ea10257
reza <myrezabehboodi@gmail.com>
2017-05-19 10:46:16 -0700

    first record

--- web/models.py
+++ web/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
