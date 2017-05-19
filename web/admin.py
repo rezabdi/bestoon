2cb781101f5ee47611d6afe16467e891d2bfd618
reza <myrezabehboodi@gmail.com>
2017-05-19 11:27:54 -0700

    after adding requirments.txt

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from models import Expense
+from .models import Expense , Income
+
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Expense)
+admin.site.register(Income)
--- web/models.py
+++ web/models.py
@@ -9,3 +9,11 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "in :----{}---- ur Expesne is {}".format(self.date,self.amount)
+
+class Income(models.Model):
+        text = models.CharField(max_length=255)
+        date = models.DateTimeField()
+        amount = models.BigIntegerField()
+        user = models.ForeignKey(User)
